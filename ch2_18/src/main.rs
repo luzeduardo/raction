2063aaad24281072d17f78be453659ef70a47dae
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-02 00:27:10 +0000

    print the number of the line that matches the search

--- ch2_18/src/main.rs
+++ ch2_18/src/main.rs
@@ -2,18 +2,20 @@ fn main() {
     let search_term = "picture";
 
     let quote = "\
-    Every face, every shop, bedroom window, 
-    public-house, and dark square is a picture 
+    Every face, every shop, bedroom window,
+    public-house, and dark square is a picture
     feverishly turned--in search of what?
     It is the same with books.
     What do ew seek through millions of pages?
     ";
 
-    //lines() returns a iterator over quote 
+    //lines() returns a iterator over quote
     //  where each iteration is a line of text
+    let mut line_num: usize = 1;
     for line in quote.lines() {
         if line.contains(search_term) {
-            println!("{}", line);
+            println!("{}: {}", line_num, line);
         }
+        line_num += 1;
     }
 }

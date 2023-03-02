c94a6ae3fd094aa44aba13b742c0974a95e72d23
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-02 00:27:11 +0000

    implementing a short-lived variables strategy

--- ch4_8/src/main.rs
+++ ch4_8/src/main.rs
@@ -1,29 +1,54 @@
-type Message = String;
 #[derive(Debug)]
-struct MailBox {
+struct Message {
+    to: u64,
+    content: String,
+}
+#[derive(Debug)]
+struct Mailbox {
     messages: Vec<Message>,
+}
+
+impl Mailbox {
+    /// requires mutable access to itself and ownership over a message
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                // when find a message returns early with the Message wrapped in Some Option
+                return Some(msg);
+            }
+        }
+        // when no messages are found, returns None
+        None
+    }
+    
 }
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
-    mailbox: MailBox,
 }
 
 struct GroundStation;
 impl GroundStation {
     /// the &self indicates that groundstation.send only requires a read only ref to self.
-    fn send(&self, to: &mut CubeSat, msg: Message,) {
-        to.mailbox.messages.push(msg);
+    fn send(&self, mailbox: &mut Mailbox, msg: Message,) {
+        // yeilds ownership of a message
+        mailbox.post(msg); 
     }
 
     fn connect(&self, sat_id: u64) -> CubeSat {
-        CubeSat { id: sat_id, mailbox: MailBox { messages: vec![] } }
+        CubeSat { id: sat_id }
     }
 }
 
 impl CubeSat {
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        // calls Mailbox.deliver to receive msgs, gaining ownership of a message
+        mailbox.deliver(&self)
     }
 }
 
@@ -43,18 +68,22 @@ fn fetch_sat_ids() -> Vec<u64> {
 // }
 
 fn main() {
+    let mut mail = Mailbox { messages: vec![] };
+
     let base = GroundStation {};
-    let mut sat_a = CubeSat {
-        id: 0,
-        mailbox: MailBox { messages: vec![], },
-    };
+    let sat_ids = fetch_sat_ids();
 
-    println!("t0: {:?}", sat_a);
-    base.send(&mut sat_a, Message::from("hello there!"));
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = Message { to: sat_id, content: String::from("hello!") };
+        base.send(&mut mail, msg);
+    }
 
-    println!("t1: {:?}", sat_a);
+    let sat_ids = fetch_sat_ids();
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
 
-    let msg = sat_a.recv();
-
-    println!("msg: {:?}", msg);
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
 }

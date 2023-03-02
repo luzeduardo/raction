08947724dfbde23f04b098975bc4b39434bc2781
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-02 00:27:12 +0000

    adding logic to perform the opcde

--- ch5_18/src/main.rs
+++ ch5_18/src/main.rs
@@ -3,6 +3,32 @@ struct CPU {
     registers: [u8; 2], // only tw registers needed for addition
 }
 
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        self.current_operation
+    }
+
+    fn run(&mut self) {
+        // loop {
+            let opcode = self.read_opcode();
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
+
+            match (c, x, y, d) {
+                // dipatches execution to hardware circuit to be performed
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
+        // }
+    }
+
+    fn add_xy(&mut self, x: u8, y:u8) {
+        self.registers[x as usize] += self.registers[y as usize];
+    }
+}
+
 fn main() {
     let mut cpu = CPU {
         current_operation: 0, // initialize with no op

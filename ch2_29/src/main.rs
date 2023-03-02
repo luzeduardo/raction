2207e071e3bec4e01b1870beba81af3d2269e17c
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-02 00:27:10 +0000

    reading lines from a file

---
+++ ch2_29/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use regex::Regex;
+use clap::{App,Arg};
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searchs for patterns")
+        .arg(Arg::with_name("pattern")
+        .help("The pattern to search for")
+        .takes_value(true)
+        .required(true))
+    .arg(Arg::with_name("input")
+        .help("File to search")
+        .takes_value(true)
+        .required(true))
+    .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+
+}

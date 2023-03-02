87b8b9e2cb4590e4bb9db3cdfe40a60e87c2a183
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-02 00:27:10 +0000

    adding support to grep-lite to read from args

--- ch2_23/src/main.rs
+++ ch2_23/src/main.rs
@@ -1,23 +1,17 @@
 use regex::Regex;
+use clap::{App,Arg};
 fn main() {
-    let search_term = "picture";
-    let haystack = "\
-    Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books. 
-    What do we seek through millions of pages?";
+    let args = App::new("grep-lite")
+    .version("0.1")
+    .about("searches for patterns")
+    .arg(Arg::with_name("pattern")
+    .help("The pattern to search for")
+    .takes_value(true)
+    .required(true))
+    .get_matches();
 
-    for line in haystack.lines() {
-        if line.contains(search_term) {
-            println!("contains match: {}", line);
-        }
-    }
-
-    find_with_regex();
-}
-
-fn find_with_regex() {
-    let re = Regex::new("picture").unwrap();
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
@@ -25,11 +19,10 @@ fn find_with_regex() {
     What do we seek through millions of pages?";
 
     for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+        match re.find(line) {
             //Some(T) is the positive case of an Ooption
             //meaning successful
-            Some(_) => println!("regex match: {}", line),
+            Some(_) => println!("{}", line),
             None => (),
         }
     }

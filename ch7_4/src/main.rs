c42cbab2334d1ecceb25065af0caa9fe92629728
Eduardo Luz <luz.eduardo@gmail.com>
2023-03-05 22:51:57 +0000

    using std::path::Path and std::String to extract a file parent dir and the main differences

--- ch7_4/src/main.rs
+++ ch7_4/src/main.rs
@@ -1,36 +1,11 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::env;
-
-const BYTES_PER_LINE: usize = 16;
+use std::path::PathBuf;
 fn main() {
-    // handle command line args without 3rd libs
-    // it returns an iterator with an nth method that extract the nth position
-    // every iterator  nth returns an Option
-    let arg1 = env::args().nth(1);
+    let hello = String::from("/tmp/hello.txt");
+    let tmp_dir = hello.split("/").nth(0);
+    println!("{:?}", tmp_dir); //prints Some("")
 
-    // the expect is considered a friendlier version of unwrap
-    // it takes an error message as arument whereas unwrap simply panics
-    let fname = arg1.expect("usage: fview FILENAME");
-
-    let mut f = File::open(&fname).expect("Unable to open file.");
-    let mut pos = 0;
-    let mut buffer = [0; BYTES_PER_LINE];
-
-    //goes out of loop when f.read_exact 
-    //returns Err when it run out of bytes to read
-    // read_exact from Read trait transfers data from the source to the buffer
-    // provided as argument. it stops when the buffer is full
-    while let Ok(_) = f.read_exact(&mut buffer) {
-        print!("[0x{:08x}] ", pos);
-        for byte in &buffer {
-            match *byte {
-                0x00 => print!(".  "),
-                0xff => print!("## "),
-                _ => print!("{:02x} ", byte),
-            }
-        }
-        println!("");
-        pos += BYTES_PER_LINE;
-    }
+    // Path provide dedicated methods for common operations such as setting a file etension
+    let mut path_buff_hello = PathBuf::from("/tmp/hello.txt");
+    path_buff_hello.pop();
+    println!("{:?}", path_buff_hello.display());
 }
